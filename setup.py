17cc53efb18bf67f5b1ae9ee0eb820cbd63bebb3
aburtonProto <aburton@protosthetics.com>
2021-10-28 14:44:56 -0500

    somehow gpiozero made it need old python

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/aburtonProto/OctoPrint-Helloworld"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["gpiozero>=1.5"]
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
