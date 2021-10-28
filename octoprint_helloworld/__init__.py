7fb99911fb65c4765931e4e5bcb4abc81c60f2d4
Aaron Burton <aburton@protosthetics.com>
2021-10-28 18:02:13 +0100

    initial commit

---
+++ octoprint_helloworld/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, unicode_literals
+
+import octoprint.plugin
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
+  def on_after_startup(self):
+    self._logger.info("hello world!!!")
+
+  def get_settings_defaults(self):
+    return dict(words="Is it Christmas?")
+
+  def get_template_vars(self):
+    return dict(words=self._settings.get(["words"]))
+
+  def get_template_configs(self):
+    return [
+      dict(type="navbar", custom_bindings=False),
+      dict(type="settings", custom_bindings=False)
+    ]
+
+__plugin_name__ = "Hey there"
+__plugin_pythoncompat__ = ">=3,<4"
+__plugin_implementation__ = HelloWorldPlugin()
+
