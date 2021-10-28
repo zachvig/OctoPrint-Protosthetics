6046cc0443bd8dfe26604e3baf07f891dcfacc68
aburtonProto <aburton@protosthetics.com>
2021-10-28 14:34:25 -0500

    requirements (again)

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/aburtonProto/OctoPrint-Helloworld"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["python3-gpiozero"]
+plugin_requires = ["gpiozero>=1.5"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
