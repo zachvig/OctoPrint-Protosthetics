eba51910313682211ecab9c0448d0aa1a659609e
aburtonProto <aburton@protosthetics.com>
2021-11-01 16:55:29 -0500

    attempting what Charlie_Powell suggested

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -27,7 +27,7 @@ class HelloWorldPlugin(octoprint.plugin.TemplatePlugin,
 
   def get_template_configs(self):
     return [
-      dict(type="navbar", custom_bindings=False, data_bind=self.test),
+      dict(type="navbar"),
       dict(type="settings", custom_bindings=False)
     ]
   
